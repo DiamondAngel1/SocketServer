97b8bd308354a4b08f99f2ca6dc594e38718f670
DiamondAngel1 <tararukolga78@gmail.com>
2025-05-15 17:12:52 +0300

    first commit

---
+++ SocketClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+client.Connect("127.0.0.1",2009);
+
+string message = Console.ReadLine();
+
+client.Send(Encoding.UTF8.GetBytes(message));
+
+byte[] buffer = new byte[1024];
+int bytes = client.Receive(buffer);
+Console.WriteLine($"Відповідь від сервера " + Encoding.UTF8.GetString(buffer));
+
+client.Shutdown(SocketShutdown.Both);
+client.Close();
---
+++ SocketServer/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var endPoint = new IPEndPoint(IPAddress.Any, 2009);
+server.Bind(endPoint);
+
+server.Listen(10);
+
+Console.WriteLine($"Запуск сервера {endPoint}");
+
+Socket client = server.Accept();
+Console.WriteLine($"Клієнт підключився {client.RemoteEndPoint}");
+
+var buffer =  new byte[1024];
+int sizeBytes = client.Receive(buffer);
+
+var text = Encoding.UTF8.GetString(buffer, 0, sizeBytes);
+Console.WriteLine("Отримали: " + text);
+string resultText = "Дякую козаче!";
+client.Send(Encoding.UTF8.GetBytes(resultText));
+
+client.Shutdown(SocketShutdown.Both);
+client.Close();
+server.Close();
